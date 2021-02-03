8e30d489629c4f638173d1cf04fd8c64c559f4af
arkiver <arkiver@protonmail.com>
2021-02-03 04:31:27 +0100

    initial

---
+++ halo.lua
@@ -0,0 +1,333 @@
+dofile("table_show.lua")
+dofile("urlcode.lua")
+local urlparse = require("socket.url")
+local http = require("socket.http")
+JSON = (loadfile "JSON.lua")()
+
+local item_dir = os.getenv('item_dir')
+local warc_file_base = os.getenv('warc_file_base')
+local item_type = nil
+local item_name = nil
+local item_value = nil
+
+local url_count = 0
+local tries = 0
+local downloaded = {}
+local addedtolist = {}
+local abortgrab = false
+local exit_url = false
+
+local discovered = {}
+
+local bad_items = {}
+
+if not urlparse or not http then
+  io.stdout:write("socket not corrently installed.\n")
+  io.stdout:flush()
+  abortgrab = true
+end
+
+local ids = {}
+
+for ignore in io.open("ignore-list", "r"):lines() do
+  downloaded[ignore] = true
+end
+
+abort_item = function(item)
+  abortgrab = true
+  if not item then
+    item = item_name
+  end
+  if not bad_items[item] then
+    io.stdout:write("Aborting item " .. item .. ".\n")
+    io.stdout:flush()
+    bad_items[item] = true
+  end
+end
+
+read_file = function(file)
+  if file then
+    local f = assert(io.open(file))
+    local data = f:read("*all")
+    f:close()
+    return data
+  else
+    return ""
+  end
+end
+
+allowed = function(url, parenturl)
+  if string.match(url, "[Pp]layer[Gg]ame[Hh]istory%.aspx")
+    or not string.match(url, "^https?://[^/]*bungie%.net/") then
+    return false
+  end
+
+  local tested = {}
+  for s in string.gmatch(url, "([^/]+)") do
+    if not tested[s] then
+      tested[s] = 0
+    end
+    if tested[s] == 6 then
+      return false
+    end
+    tested[s] = tested[s] + 1
+  end
+
+  if string.match(url, "/[Ss]creenshot%.ashx")
+    and item_type == "reach-file" then
+    return true
+  end
+
+  local match = string.match(url, "%?player=([^&]+)$")
+  if match then
+    if item_type == "player" then
+      if urlparse.unescape(match) == item_value then
+        return true
+      end
+    else
+      discovered["player:" .. match] = true
+    end
+  end
+
+  for s in string.gmatch(url, "([0-9]+)") do
+    if ids[s] then
+      return true
+    end
+  end
+
+  match = string.match(url, "/[Gg]ame[Ss]tats%.aspx%?guid=([0-9]+)$")
+  if match then
+    discovered["reach-guid:" .. match] = true
+  end
+
+  match = string.match(url, "/default%.aspx%?pxd=([^&]+)$")
+  if match then
+    discovered["reach-pxd:" .. match] = true
+  end
+
+  return false
+end
+
+wget.callbacks.download_child_p = function(urlpos, parent, depth, start_url_parsed, iri, verdict, reason)
+  return false
+end
+
+wget.callbacks.get_urls = function(file, url, is_css, iri)
+  local urls = {}
+  local html = nil
+  
+  downloaded[url] = true
+
+  local function check(urla)
+    local origurl = url
+    local url = string.match(urla, "^([^#]+)")
+    local url_ = string.match(url, "^(.-)%.?$")
+    url_ = string.gsub(url_, "&amp;", "&")
+    url_ = string.match(url_, "^(.-)%s*$")
+    url_ = string.match(url_, "^(.-)%??$")
+    url_ = string.match(url_, "^(.-)&?$")
+    url_ = string.match(url_, "^(.-)/?$")
+    if (downloaded[url_] ~= true and addedtolist[url_] ~= true)
+      and allowed(url_, origurl) then
+      table.insert(urls, { url=url_ })
+      addedtolist[url_] = true
+      addedtolist[url] = true
+    end
+  end
+
+  local function checknewurl(newurl)
+    if string.match(newurl, "\\[uU]002[fF]") then
+      return checknewurl(string.gsub(newurl, "\\[uU]002[fF]", "/"))
+    end
+    if string.match(newurl, "^https?:////") then
+      check(string.gsub(newurl, ":////", "://"))
+    elseif string.match(newurl, "^https?://") then
+      check(newurl)
+    elseif string.match(newurl, "^https?:\\/\\?/") then
+      check(string.gsub(newurl, "\\", ""))
+    elseif string.match(newurl, "^\\/") then
+      checknewurl(string.gsub(newurl, "\\", ""))
+    elseif string.match(newurl, "^//") then
+      check(urlparse.absolute(url, newurl))
+    elseif string.match(newurl, "^/") then
+      check(urlparse.absolute(url, newurl))
+    elseif string.match(newurl, "^%.%./") then
+      if string.match(url, "^https?://[^/]+/[^/]+/") then
+        check(urlparse.absolute(url, newurl))
+      else
+        checknewurl(string.match(newurl, "^%.%.(/.+)$"))
+      end
+    elseif string.match(newurl, "^%./") then
+      check(urlparse.absolute(url, newurl))
+    end
+  end
+
+  local function checknewshorturl(newurl)
+    if string.match(newurl, "^%?") then
+      check(urlparse.absolute(url, newurl))
+    elseif not (string.match(newurl, "^https?:\\?/\\?//?/?")
+      or string.match(newurl, "^[/\\]")
+      or string.match(newurl, "^%./")
+      or string.match(newurl, "^[jJ]ava[sS]cript:")
+      or string.match(newurl, "^[mM]ail[tT]o:")
+      or string.match(newurl, "^vine:")
+      or string.match(newurl, "^android%-app:")
+      or string.match(newurl, "^ios%-app:")
+      or string.match(newurl, "^%${")) then
+      check(urlparse.absolute(url, "/" .. newurl))
+    end
+  end
+
+  if allowed(url, nil) and status_code == 200 then
+    html = read_file(file)
+    for newurl in string.gmatch(string.gsub(html, "&quot;", '"'), '([^"]+)') do
+      checknewurl(newurl)
+    end
+    for newurl in string.gmatch(string.gsub(html, "&#039;", "'"), "([^']+)") do
+      checknewurl(newurl)
+    end
+    for newurl in string.gmatch(html, ">%s*([^<%s]+)") do
+      checknewurl(newurl)
+    end
+    for newurl in string.gmatch(html, "[^%-]href='([^']+)'") do
+      checknewshorturl(newurl)
+    end
+    for newurl in string.gmatch(html, '[^%-]href="([^"]+)"') do
+      checknewshorturl(newurl)
+    end
+    for newurl in string.gmatch(html, ":%s*url%(([^%)]+)%)") do
+      checknewurl(newurl)
+    end
+  end
+
+  return urls
+end
+
+wget.callbacks.httploop_result = function(url, err, http_stat)
+  status_code = http_stat["statcode"]
+
+  local match = string.match(url["url"], "^https?://halo%.bungie%.net/Stats/Reach/FileDetails%.aspx%?fid=([0-9]+)$")
+  local type_ = "reach-file"
+  if not match then
+    match = string.match(url["url"], "^https?://halo%.bungie%.net/Stats/Reach/GameStats%.aspx%?guid=([0-9]+)$")
+    type_ = "reach-guid"
+  end
+  if not match then
+    match = string.match(url["url"], "^https?://halo%.bungie%.net/Stats/Reach/GameStats%.aspx%?gameid=([0-9]+)$")
+    type_ = "reach-stats"
+  end
+  if not match then
+    match = string.match(url["url"], "^https?://halo%.bungie%.net/Stats/Reach/default%.aspx%?player=([^&]+)$")
+    type_ = "reach-player"
+  end
+  if not match then
+    match = string.match(url["url"], "^https?://halo%.bungie%.net/Stats/Reach/default%.aspx%?pxd=([^&]+)$")
+    type_ = "reach-pxd"
+  end
+  if match then
+    abortgrab = false
+    ids[match] = true
+    item_value = match
+    item_type = type_
+    item_name = type_ .. ":" .. match
+    io.stdout:write("Archiving item " .. item_name .. ".\n")
+    io.stdout:flush()
+  end
+  
+  url_count = url_count + 1
+  io.stdout:write(url_count .. "=" .. status_code .. " " .. url["url"] .. "  \n")
+  io.stdout:flush()
+
+  if status_code >= 300 and status_code <= 399 then
+    local newloc = urlparse.absolute(url["url"], http_stat["newloc"])
+    if downloaded[newloc] or addedtolist[newloc]
+      or not allowed(newloc, url["url"]) then
+      tries = 0
+      return wget.actions.EXIT
+    end
+  end
+  
+  if status_code >= 200 and status_code <= 399 then
+    downloaded[url["url"]] = true
+  end
+
+  if abortgrab then
+    abort_item()
+    return wget.actions.EXIT
+  end
+
+  if status_code == 0
+    or (status_code > 400 and status_code ~= 404) then
+    io.stdout:write("Server returned " .. http_stat.statcode .. " (" .. err .. "). Sleeping.\n")
+    io.stdout:flush()
+    local maxtries = 1
+    if not allowed(url["url"], nil) then
+      maxtries = 3
+    end
+    if tries >= maxtries then
+      io.stdout:write("I give up...\n")
+      io.stdout:flush()
+      tries = 0
+      abort_item()
+      return wget.actions.EXIT
+    else
+      os.execute("sleep " .. math.floor(math.pow(2, tries)))
+      tries = tries + 1
+      return wget.actions.CONTINUE
+    end
+  end
+
+  tries = 0
+
+  local sleep_time = 0
+
+  if sleep_time > 0.001 then
+    os.execute("sleep " .. sleep_time)
+  end
+
+  return wget.actions.NOTHING
+end
+
+wget.callbacks.finish = function(start_time, end_time, wall_time, numurls, total_downloaded_bytes, total_download_time)
+  local file = io.open(item_dir .. '/' .. warc_file_base .. '_bad-items.txt', 'w')
+  for url, _ in pairs(bad_items) do
+    file:write(url .. "\n")
+  end
+  file:close()
+  local items = nil
+  for item, _ in pairs(discovered) do
+    print('found item', item)
+    if items == nil then
+      items = item
+    else
+      items = items .. "\0" .. item
+    end
+  end
+  if items ~= nil then
+    local tries = 0
+    while tries < 10 do
+      local body, code, headers, status = http.request(
+        "http://blackbird-amqp.meo.ws:23038/halo-new-pzp644wwf6omdpc/",
+        items
+      )
+      if code == 200 or code == 409 then
+        break
+      end
+      io.stdout:write("Could not queue new items.\n")
+      io.stdout:flush()
+      os.execute("sleep " .. math.floor(math.pow(2, tries)))
+      tries = tries + 1
+    end
+    if tries == 10 then
+      abortgrab = true
+    end
+  end
+end
+
+wget.callbacks.before_exit = function(exit_status, exit_status_string)
+  if abortgrab then
+    abort_item()
+  end
+  return exit_status
+end
+
---
+++ pipeline.py
@@ -0,0 +1,335 @@
+# encoding=utf8
+import datetime
+from distutils.version import StrictVersion
+import hashlib
+import os.path
+import random
+from seesaw.config import realize, NumberConfigValue
+from seesaw.externalprocess import ExternalProcess
+from seesaw.item import ItemInterpolation, ItemValue
+from seesaw.task import SimpleTask, LimitConcurrent
+from seesaw.tracker import GetItemFromTracker, PrepareStatsForTracker, \
+    UploadWithTracker, SendDoneToTracker
+import shutil
+import socket
+import subprocess
+import sys
+import time
+import string
+
+import seesaw
+from seesaw.externalprocess import WgetDownload
+from seesaw.pipeline import Pipeline
+from seesaw.project import Project
+from seesaw.util import find_executable
+
+if StrictVersion(seesaw.__version__) < StrictVersion('0.8.5'):
+    raise Exception('This pipeline needs seesaw version 0.8.5 or higher.')
+
+
+###########################################################################
+# Find a useful Wget+Lua executable.
+#
+# WGET_AT will be set to the first path that
+# 1. does not crash with --version, and
+# 2. prints the required version string
+
+WGET_AT = find_executable(
+    'Wget+AT',
+    ['GNU Wget 1.20.3-at.20201030.01'],
+    ['./wget-at']
+)
+
+if not WGET_AT:
+    raise Exception('No usable Wget+At found.')
+
+
+###########################################################################
+# The version number of this pipeline definition.
+#
+# Update this each time you make a non-cosmetic change.
+# It will be added to the WARC files and reported to the tracker.
+VERSION = '20210203.01'
+USER_AGENT = 'Archive Team'
+TRACKER_ID = 'halo-new'
+TRACKER_HOST = 'trackerproxy.archiveteam.org'
+MULTI_ITEM_SIZE = 5
+
+
+###########################################################################
+# This section defines project-specific tasks.
+#
+# Simple tasks (tasks that do not need any concurrency) are based on the
+# SimpleTask class and have a process(item) method that is called for
+# each item.
+class CheckIP(SimpleTask):
+    def __init__(self):
+        SimpleTask.__init__(self, 'CheckIP')
+        self._counter = 0
+
+    def process(self, item):
+        # NEW for 2014! Check if we are behind firewall/proxy
+
+        if self._counter <= 0:
+            item.log_output('Checking IP address.')
+            ip_set = set()
+
+            ip_set.add(socket.gethostbyname('twitter.com'))
+            ip_set.add(socket.gethostbyname('facebook.com'))
+            ip_set.add(socket.gethostbyname('youtube.com'))
+            ip_set.add(socket.gethostbyname('microsoft.com'))
+            ip_set.add(socket.gethostbyname('icanhas.cheezburger.com'))
+            ip_set.add(socket.gethostbyname('archiveteam.org'))
+
+            if len(ip_set) != 6:
+                item.log_output('Got IP addresses: {0}'.format(ip_set))
+                item.log_output(
+                    'Are you behind a firewall/proxy? That is a big no-no!')
+                raise Exception(
+                    'Are you behind a firewall/proxy? That is a big no-no!')
+
+        # Check only occasionally
+        if self._counter <= 0:
+            self._counter = 10
+        else:
+            self._counter -= 1
+
+
+class PrepareDirectories(SimpleTask):
+    def __init__(self, warc_prefix):
+        SimpleTask.__init__(self, 'PrepareDirectories')
+        self.warc_prefix = warc_prefix
+
+    def process(self, item):
+        item_name = item['item_name']
+        item_name_hash = hashlib.sha1(item_name.encode('utf8')).hexdigest()
+        escaped_item_name = item_name_hash
+        dirname = '/'.join((item['data_dir'], escaped_item_name))
+
+        if os.path.isdir(dirname):
+            shutil.rmtree(dirname)
+
+        os.makedirs(dirname)
+
+        item['item_dir'] = dirname
+        item['warc_file_base'] = '-'.join([
+            self.warc_prefix,
+            item_name_hash,
+            time.strftime('%Y%m%d-%H%M%S')
+        ])
+
+        open('%(item_dir)s/%(warc_file_base)s.warc.gz' % item, 'w').close()
+        open('%(item_dir)s/%(warc_file_base)s_bad-items.txt' % item, 'w').close()
+        open('%(item_dir)s/%(warc_file_base)s_data.txt' % item, 'w').close()
+
+class MoveFiles(SimpleTask):
+    def __init__(self):
+        SimpleTask.__init__(self, 'MoveFiles')
+
+    def process(self, item):
+        os.rename('%(item_dir)s/%(warc_file_base)s.warc.gz' % item,
+              '%(data_dir)s/%(warc_file_base)s.warc.gz' % item)
+        os.rename('%(item_dir)s/%(warc_file_base)s_data.txt' % item,
+              '%(data_dir)s/%(warc_file_base)s_data.txt' % item)
+
+        shutil.rmtree('%(item_dir)s' % item)
+
+
+class SetBadUrls(SimpleTask):
+    def __init__(self):
+        SimpleTask.__init__(self, 'SetBadUrls')
+
+    def process(self, item):
+        item['item_name_original'] = item['item_name']
+        items = item['item_name'].split('\0')
+        items_lower = [s.lower() for s in items]
+        with open('%(item_dir)s/%(warc_file_base)s_bad-items.txt' % item, 'r') as f:
+            for aborted_item in f:
+                aborted_item = aborted_item.strip().lower()
+                index = items_lower.index(aborted_item)
+                item.log_output('Item {} is aborted.'.format(aborted_item))
+                items.pop(index)
+                items_lower.pop(index)
+        item['item_name'] = '\0'.join(items)
+
+
+class MaybeSendDoneToTracker(SendDoneToTracker):
+    def enqueue(self, item):
+        if len(item['item_name']) == 0:
+            return self.complete_item(item)
+        return super(MaybeSendDoneToTracker, self).enqueue(item)
+
+
+def get_hash(filename):
+    with open(filename, 'rb') as in_file:
+        return hashlib.sha1(in_file.read()).hexdigest()
+
+CWD = os.getcwd()
+PIPELINE_SHA1 = get_hash(os.path.join(CWD, 'pipeline.py'))
+LUA_SHA1 = get_hash(os.path.join(CWD, 'halo.lua'))
+
+def stats_id_function(item):
+    d = {
+        'pipeline_hash': PIPELINE_SHA1,
+        'lua_hash': LUA_SHA1,
+        'python_version': sys.version,
+    }
+
+    return d
+
+
+class WgetArgs(object):
+    def realize(self, item):
+        wget_args = [
+            WGET_AT,
+            '-U', USER_AGENT,
+            '-nv',
+            '--content-on-error',
+            '--lua-script', 'halo.lua',
+            '-o', ItemInterpolation('%(item_dir)s/wget.log'),
+            '--no-check-certificate',
+            '--output-document', ItemInterpolation('%(item_dir)s/wget.tmp'),
+            '--truncate-output',
+            '-e', 'robots=off',
+            '--rotate-dns',
+            '--recursive', '--level=inf',
+            '--no-parent',
+            '--page-requisites',
+            '--timeout', '30',
+            '--tries', 'inf',
+            '--domains', 'bungie.net',
+            '--span-hosts',
+            '--waitretry', '30',
+            '--warc-file', ItemInterpolation('%(item_dir)s/%(warc_file_base)s'),
+            '--warc-header', 'operator: Archive Team',
+            '--warc-header', 'x-wget-at-project-version: ' + VERSION,
+            '--warc-header', 'x-wget-at-project-name: ' + TRACKER_ID,
+            '--warc-dedup-url-agnostic',
+        ]
+
+        def queue_range(start, end, url_prefix, header_prefix):
+            for i in range(start, end+1):
+                wget_args.extend(['--warc-header', header_prefix+': '+str(i)])
+                wget_args.append(url_prefix+str(i))
+
+        def process_sequential(s, url_prefix, header_prefix):
+            if '-' in s:
+                start, end = s.split('-')
+            else:
+                start, end = int(s), int(s)
+            queue_range(int(start), int(end), url_prefix, header_prefix)
+
+        item_names = item['item_name'].split('\0')
+
+        for item_name in item_names:
+            wget_args.extend(['--warc-header', 'x-wget-at-project-item-name: '+item_name])
+            wget_args.append('item-name://'+item_name)
+            item_type, item_value = item_name.split(':', 1)
+            if item_type == 'reach-file':
+                process_sequential(
+                    item_value,
+                    'http://halo.bungie.net/Stats/Reach/FileDetails.aspx?fid=',
+                    'halo-bungie-reach-file'
+                )
+            elif item_type == 'reach-guid':
+                wget_args.extend(['--warc-header', 'halo-bungie-reach-game-guid: '+item_value])
+                wget_args.append('http://halo.bungie.net/Stats/Reach/GameStats.aspx?guid='+item_value)
+            elif item_type == 'reach-stats':
+                process_sequential(
+                    item_value,
+                    'http://halo.bungie.net/Stats/Reach/GameStats.aspx?gameid=',
+                    'halo-bungie-reach-game-stats'
+                )
+            elif item_type in ('player', 'reach-player'):
+                item_names.remove(item_name)
+            #    wget_args.extend(['--warc-header', 'halo-bungie-player: '+item_value])
+            #    wget_args.extend(['--warc-header', 'halo-bungie-reach-player: '+item_value])
+            #    wget_args.extend(['--warc-header', 'halo-bungie-halo2-player: '+item_value])
+            #    wget_args.extend(['--warc-header', 'halo-bungie-halo3-player: '+item_value])
+            #    wget_args.append('http://halo.bungie.net/Stats/Reach/default.aspx?player='+item_value)
+            #    wget_args.append('http://halo.bungie.net/Stats/PlayerStatsHalo2.aspx?player='+item_value)
+            #    wget_args.append('http://halo.bungie.net/Stats/Halo3/default.aspx?player='+item_value)
+            elif item_type == 'reach-pxd':
+                wget_args.extend(['--warc-header', 'halo-bungie-reach-player: '+item_value])
+                wget_args.append('http://halo.bungie.net/Stats/Reach/default.aspx?pxd='+item_value)
+            else:
+                raise ValueError('item_type not supported.')
+
+        item['item_name'] = '\0'.join(item_names)
+        item['item_name_newline'] = '\n'.join(item_names)
+
+        if 'bind_address' in globals():
+            wget_args.extend(['--bind-address', globals()['bind_address']])
+            print('')
+            print('*** Wget will bind address at {0} ***'.format(
+                globals()['bind_address']))
+            print('')
+
+        return realize(wget_args, item)
+
+###########################################################################
+# Initialize the project.
+#
+# This will be shown in the warrior management panel. The logo should not
+# be too big. The deadline is optional.
+project = Project(
+    title = 'halo',
+    project_html = '''
+    <img class="project-logo" alt="logo" src="https://wiki.archiveteam.org/images/8/80/Bungie_Logo_white_background.png" height="50px"/>
+    <h2>halo.bungie.net <span class="links"><a href="https://halo.bungie.net/">Website</a> &middot; <a href="http://tracker.archiveteam.org/halo-new/">Leaderboard</a></span></h2>
+    '''
+)
+
+pipeline = Pipeline(
+    CheckIP(),
+    GetItemFromTracker('http://{}/{}/multi={}/'
+        .format(TRACKER_HOST, TRACKER_ID, MULTI_ITEM_SIZE),
+        downloader, VERSION),
+    PrepareDirectories(warc_prefix='halo'),
+    WgetDownload(
+        WgetArgs(),
+        max_tries=1,
+        accept_on_exit_code=[0, 4, 8],
+        env={
+            'item_dir': ItemValue('item_dir'),
+            'warc_file_base': ItemValue('warc_file_base'),
+        }
+    ),
+    SetBadUrls(),
+    PrepareStatsForTracker(
+        defaults={'downloader': downloader, 'version': VERSION},
+        file_groups={
+            'data': [
+                ItemInterpolation('%(item_dir)s/%(warc_file_base)s.warc.gz')
+            ]
+        },
+        id_function=stats_id_function,
+    ),
+    MoveFiles(),
+    LimitConcurrent(NumberConfigValue(min=1, max=20, default='2',
+        name='shared:rsync_threads', title='Rsync threads',
+        description='The maximum number of concurrent uploads.'),
+        UploadWithTracker(
+            'http://%s/%s' % (TRACKER_HOST, TRACKER_ID),
+            downloader=downloader,
+            version=VERSION,
+            files=[
+                ItemInterpolation('%(data_dir)s/%(warc_file_base)s.warc.gz'),
+                ItemInterpolation('%(data_dir)s/%(warc_file_base)s_data.txt')
+            ],
+            rsync_target_source_path=ItemInterpolation('%(data_dir)s/'),
+            rsync_extra_args=[
+                '--recursive',
+                '--partial',
+                '--partial-dir', '.rsync-tmp',
+                '--min-size', '1',
+                '--no-compress',
+                '--compress-level', '0'
+            ]
+        ),
+    ),
+    MaybeSendDoneToTracker(
+        tracker_url='http://%s/%s' % (TRACKER_HOST, TRACKER_ID),
+        stats=ItemValue('stats')
+    )
+)
